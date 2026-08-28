922fd99a7aaa1b1baacdf8feb085e4a6ada4b2c3
Adonissys <adonisfilipe1@gmail.com>
2026-08-28 19:24:11 -0300

    Rest mechanic
    update

---
+++ Scripts/JailScene.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class JailScene : Node2D
+{
+	[Export]private CharacterBody2D Player;
+	[Export]private Area2D RestButton;
+	
+	[Signal] public delegate void ActionEventHandler();
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		RestButton.InputEvent += OnRestButtonInput;
+		TrainingButton.InputEvent += OnTrainingButtonInput;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnRestButtonInput(Node viewport, InputEvent @event, long shapeIdx){
+		if(@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left){
+			Rest(1);
+			GD.Print("Fui clicado");
+		}
+			
+	}
+	
+	private void Rest(int heal){
+		if(true){ //fazer avaliar a quantidade de ações futuramente
+			// linha em que o jogador recebe vida pelo descanso
+			EmitSignal(SignalName.Action);
+			}
+	}
+
+}
