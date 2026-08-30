afa2ea475505d663ac971349108cdc3c9fb4b653
JoaoMiguelDev <jmsabarreto67@gmail.com>
2026-08-30 17:11:10 -0300

    player fixed

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,32 +7,26 @@ public partial class Player : CharacterBody2D
 	[Signal] public delegate void DeathEventHandler();
 
 	[Export] public Enemy CurrentEnemy;
-
 	[Export] public AnimatedSprite2D PlayerSprite;
 	
 	public int Health
 	{
 	    get { return Global.Instance.Health; }
 	    set { Global.Instance.Health = value; }
-
 	}
 	
 	private int _resistance;
 
 	public override void _Ready()
 	{
-		// CurrentEnemy.Attack += OnEnemyAttack;
+		if (CurrentEnemy != null){
+			CurrentEnemy.Attack += OnEnemyAttack;
+		}
+		PlayerSprite.AnimationFinished += OnAnimationFinished;
 		Health = Global.Instance.Health;
 		_resistance = Global.Instance.Resistance;
 
-		//PlayerAnimation.Play("idle");
-	}
-
-	public override void _Process(double delta)
-	{	
-		if (Global.Instance.Outcome == Global.FightOutcome.PLAYER_WON){
-			//Player won logic
-		}
+		PlayerSprite.Play("idle");
 	}
 
 	private void TakeDamage(int damageTaken)
@@ -51,20 +45,19 @@ public partial class Player : CharacterBody2D
 	private void HandleDeath()
 	{
 		Global.Instance.Outcome = Global.FightOutcome.ENEMY_WON;
-		//PlayerAnimation.Play("death");
+		PlayerSprite.Play("death");
 	}
 
 	private void OnEnemyAttack(int baseDamage){
 		TakeDamage(baseDamage);
 	}
 
-	private void OnAnimationFinished(StringName animName){
-		if (animName == "death"){
+	private void OnAnimationFinished(){
+		if (Global.Instance.Outcome == Global.FightOutcome.ENEMY_WON){
 			EmitSignal(SignalName.Death);
 		}else{
-			//PlayerAnimation.Play("idle");
+			PlayerSprite.Play("idle");
 		}
-
 	}
 	
 }
